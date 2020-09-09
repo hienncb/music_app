b561654b12858c13809e8e3627b4d62b4e6a59c0
unknown <hienncb>
2020-09-09 22:37:12 +0700

    'dsad'

--- src/sagas/Api.js
+++ src/sagas/Api.js
@@ -1,14 +1,15 @@
 import callApi from "./ApiCaller";
 
 // const url = 'https://nets-play-music.herokuapp.com/music/';
-const url_today = 'http://192.168.1.2:3000/music/today_music';
-const url_year = 'http://192.168.1.2:3000/music/year_music';
+const url_today = 'https://music-serverasp.herokuapp.com/music/today_music';
+const url_year = 'https://music-serverasp.herokuapp.com/music/year_music';
 
 
 function* getTodayMusics(){
     var data = [];
     yield callApi(url_today, 'GET',{data:null}).then(res =>{
-        data = res.data.today.fulfillmentValue;
+        // console.log('@@@@@@@@@@@@@@@@@@@@@', res.data.today);
+        data = res.data.today;
     })
     return data;
 }
@@ -18,7 +19,7 @@ function* getYearMusics(){
     var data = [];
     yield callApi(url_year, 'GET',{data:null}).then(res =>{
         // console.log('@@@@@@@@@@@@@@@@@@@@@', res.data.year.fulfillmentValue);
-         data = res.data.year.fulfillmentValue;
+         data = res.data.year;
     })
     return data;
 }
